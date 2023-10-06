b002d081715a996c7cb73efcc2987feb7f03344d
Sriram <srirams@pdx.edu>
2023-10-06 15:00:36 -0700

    HW1 Assignment Submission

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Sriram Subramanyam. I am enthusiastic about web  development, and I have past experience in HTML, CSS, and JavaScript from my undergraduate studies. Furthermore, I have spent the last seven years as a software engineer at TCS, where I have worked with various web technologies, such as HTML, CSS, and JavaScript."
+  );
 }
